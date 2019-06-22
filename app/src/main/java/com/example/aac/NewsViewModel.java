fa52e039ebcfe9f54bf1ffdc306b4d27bac002ba
wangqx <690333243@qq.com>
2019-06-23 04:20:23 +0800

    一、rxjava 网络请求

--- app/src/main/java/com/example/aac/NewsViewModel.java
+++ app/src/main/java/com/example/aac/NewsViewModel.java
@@ -8,8 +8,21 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
 import android.util.Log;
-
 import com.example.bean.NewsDataVo;
+import com.uber.autodispose.AutoDispose;
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
+
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.observers.ConsumerSingleObserver;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 public class NewsViewModel extends AndroidViewModel {
     private static String TAG = "NewsViewModel";
@@ -39,7 +52,7 @@ public class NewsViewModel extends AndroidViewModel {
      * 模拟获取网络数据
      */
     void httpGetData() {
-        int len = 10;
+        int len = 1000;
         for (int i = 0; i < len; i++) {
             NewsDataVo dataVo = new NewsDataVo();
             dataVo.setId("1223" + i);
@@ -47,6 +60,43 @@ public class NewsViewModel extends AndroidViewModel {
             dataVo.setNewsContent("Android Architecture Components,简称 AAC,一个处理UI的生命周期与数据的持久化的架构" + i);
             dataVo.setReadNum(i);
             mData.setValue(dataVo);
+            Log.i(TAG, "httpGetData: i="+i);
         }
+        Observable.interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+        //AutoDispose的使用就是这句
+                //.as(AutoDispose.<Long>autoDisposable(AndroidLifecycleScopeProvider.from(this)))
+                .subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Log.i("接收数据,当前线程"+Thread.currentThread().getName(), String.valueOf(aLong));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
